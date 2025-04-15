b58622430ebe40857208aae62932af107d161234
Ruben Barkow-Kuder <github@r.z11.de>
2025-04-15 05:41:51 +0200

    Fix: Specify SQLDelight plugin version in shared/build.gradle.kts
    - Set the version for com.squareup.sqldelight plugin explicitly in the plugins block of shared/build.gradle.kts (version 2.0.1)
    - This resolves plugin resolution errors and ensures compatibility with Gradle 8.x and Java 21
    - Now the shared module can be built and tested as specified in Concept_kultliederbuch.md

--- kultliederbuch/build.gradle.kts
+++ kultliederbuch/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     kotlin("multiplatform") version "1.9.23" apply false
     id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "8.2.2" apply false
-    id("com.squareup.sqldelight") version "2.0.1" apply false
 }
 
 allprojects {
