5ce7a192026e4241300c16191a77b980c4892c08
Ruben Barkow-Kuder <github@r.z11.de>
2025-04-17 05:20:51 +0200

    update gradle plugin from 8.8 to 8.11.1

--- kultliederbuch/build.gradle.kts
+++ kultliederbuch/build.gradle.kts
@@ -1,8 +1,8 @@
 // Root Gradle build script for kultliederbuch
 plugins {
     kotlin("multiplatform") version "1.9.23" apply false
-    id("com.android.application") version "8.2.2" apply false
-    id("com.android.library") version "8.2.2" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
 }
 
 allprojects {
